190143656a0de2e0bb6f5615ba10e77d490b2848
BigFish2086 <a8686.ibrahim@gmail.com>
2022-06-17 10:29:06 +0200

    update rsa.py
    Add decryption function to decrypt the blocks of the given cipher text
    to the original message provided that the given private key matches the
    public key it encrypted with

--- rsa.py
+++ rsa.py
@@ -58,3 +58,29 @@ def encrypt2(msg, n, e):
             cipher += " "
         cipher += f"{hex(pow(int(cur), e, n))}"
     return cipher
+
+
+def decrypt2(cipher, n, d):
+    n = int(n, 16)
+    d = int(d, 16)
+    msg = ""
+    tmp_msg_b16 = ""
+    for enc_num_b16 in cipher.split(" "):
+        enc_num = int(enc_num_b16, 16)
+        enc_num = int(enc_num)
+        cur = pow(enc_num, d, n)
+        msg += f"{cur}"
+        if len(tmp_msg_b16) > 0:
+            tmp_msg_b16 += " "
+        tmp_msg_b16 += f"{(hex(cur)[2:])}"
+
+    msg = int(msg)
+    msg_b16 = f"{hex(msg)[2:]}"
+    msg_b16 = msg_b16.encode()
+        return True, dec
+    except Exception as e:
+        if DEBUG:
+            print(e)
+        return False, tmp_msg_b16
+    try:
+        dec = unhexlify(msg_b16).decode("utf-8")
