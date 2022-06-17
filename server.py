f9194f60a799a88553915b9f168f3fcddb80f8e9
BigFish2086 <a8686.ibrahim@gmail.com>
2022-06-17 11:00:45 +0200

    update server.py
    Add a flexible way to send/receive to/from the client

--- server.py
+++ server.py
@@ -23,6 +23,9 @@ class Server:
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
         self.sock.listen(5)
+        self.clients = {}  # {client: [pub(e, n), username]}
+        self.talking_clients = {}  # {client: other_client}
+        self.header_len = 10  # used to detect the size of the message
         welcom_msg = "I Server started {}:{} at {}".format(self.host, self.port, time.ctime())
         print(welcom_msg)
         print("[*] Logs will be saved in {}".format(self.log_filename))
@@ -42,6 +45,30 @@ class Server:
         if self.save_logs:
             with open(self.log_filename, "a") as f:
                 f.write(data)
+
+    def custom_send(self, client, data):
+        # We must encode the message to bytes, or the receiver will not understand it
+        if isinstance(data, str):
+            data = data.encode()
+        # Encode the length of the message (as a byte)
+        message_length = f"{len(data):<{self.header_len}}".encode("utf-8")
+        client.send(message_length)
+        client.send(data)
+
+    def custom_recv(self, client):
+        try:
+            # Receive our "header" containing message length, it's size is defined and constant
+            message_header = client.recv(self.header_len)
+            if not len(message_header):
+                return False
+            # Convert header to int value
+            message_length = int(message_header.decode("utf-8").strip())
+            # Return an object of message header and message data
+            return client.recv(message_length)
+        except Exception as e:
+            if DEBUG:
+                print(e)
+            return False
 
     def listen_to_client(self, client):
         pass
